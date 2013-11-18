149555150d1717020bdb2d519d33ce5fb4bd9d0f
Aijato <jakob12@ru.is>
2013-11-18 21:40:07 +0000

    name change to main

---
+++ src/main/java/is/ru/helloworld/Main.java
@@ -0,0 +1,19 @@
+//package is.ru.helloworld;
+
+import static spark.Spark.*;
+import spark.*;
+
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      
+      get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }
+      });
+
+   }
+
+}
