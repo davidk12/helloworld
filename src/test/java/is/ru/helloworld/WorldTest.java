26045cd545d31159c04df207502a86dab841377f
davidk12 <davidk12@ru.is>
2013-11-18 16:01:14 +0000

    initial commit

---
+++ src/test/java/is/ru/helloworld/WorldTest.java
@@ -0,0 +1,13 @@
+package is.ru.helloworld;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WorldTest {
+	@Test
+	public void greetResultsInHello() {
+		World world = new World();
+		assertEquals("Hello world!", world.greet());
+	}
+}
